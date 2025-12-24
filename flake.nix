24581396f63ae2b98c82247deb4022e4817544dd
linuxury <linuxurypr@gmail.com>
2025-12-24 17:57:04 -0500

    flake.nix: pass home-manager, hyprland, niri to all hosts

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       nixosConfigurations = {
         ThinkPad = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [
             ./hosts/ThinkPad.nix
           ];
@@ -28,37 +28,37 @@
 
         Ryzen5900x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/Ryzen5900x.nix ];
         };
 
         Ryzen5800x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/Ryzen5800x.nix ];
         };
 
         ASUSTuff-A15 = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/ASUSTuff-A15.nix ];
         };
 
         Alex-Desktop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/Alex-Desktop.nix ];
         };
 
         Alex-Laptop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/Alex-Laptop.nix ];
         };
 
         generic = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit self; };
+          specialArgs = { inherit self hyprland niri home-manager; };
           modules = [ ./hosts/generic.nix ];
         };
       };
