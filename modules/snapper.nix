166dac8609ea467c2052b6af42dc7ce17899503b
linuxury <linuxurypr@gmail.com>
2025-12-24 17:21:14 -0500

    Fix snapper.nix: remove duplicate autoSnapshot, use proper config

--- modules/snapper.nix
+++ modules/snapper.nix
@@ -1,31 +1,29 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let
-  cfg = config.services.snapper.configs.root;
-in {
-  # Snapper for root (@ subvol)
-  services.snapper.configs.root = {
-    SUBVOLUME = "/";
-    ALLOW_USERS = [ "linuxury" ];
-    TIMELINE_CLEANUP = true;
-    TIMELINE_CREATE = true;
-    TIMELINE_MIN_AGE = "1800";
-    TIMELINE_LIMIT_HOURLY = "5";
-    TIMELINE_LIMIT_DAILY = "7";
-    TIMELINE_LIMIT_WEEKLY = "4";
-    TIMELINE_LIMIT_MONTHLY = "3";
-    TIMELINE_LIMIT_YEARLY = "1";
+{
+  # Snapper for root subvolume (@)
+  services.snapper = {
+    configs.root = {
+      SUBVOLUME = "/";
+      ALLOW_USERS = [ "linuxury" ];
+      TIMELINE_CLEANUP = true;
+      TIMELINE_CREATE = true;
+      TIMELINE_MIN_AGE = "1800";
+      TIMELINE_LIMIT_HOURLY = "8";
+      TIMELINE_LIMIT_DAILY = "7";
+      TIMELINE_LIMIT_WEEKLY = "4";
+      TIMELINE_LIMIT_MONTHLY = "1";
+      TIMELINE_LIMIT_YEARLY = "1";
+    };
+
+    # Auto snapshots on nixos-rebuild (pre/post)
+    autoSnapshot = true;
+    autoSnapshotCleanup = true;
+
+    # Cleanup on boot
+    cleanupTimer = true;
   };
 
-  # Pre/post snapshots on nixos-rebuild
-  services.snapper.autoSnapshot = true;
-  services.snapper.autoSnapshot.flagged = "ext4";  # Use ext4 for compatibility, even on Btrfs
-
-  # Cleanup old snapshots on boot
-  boot.postBootCommands = ''
-    ${pkgs.snapper}/bin/snapper --config root cleanup timeline
-  '';
-
-  # GRUB integration for rollback
-  boot.loader.systemd-boot.configurationLimit = 20;
+  # Limit systemd-boot entries for clean GRUB menu
+  boot.loader.systemd-boot.configurationLimit = 30;
 }
