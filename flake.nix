98727cc81fc2996c29b6b98b3b8293cec8d38e16
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 14:32:50 -0500

    Refactor hostModules and simplify specialArgs

--- flake.nix
+++ flake.nix
@@ -16,10 +16,26 @@
   outputs = { self, nixpkgs, home-manager, hyprland, niri, ... }:
     let
       system = "x86_64-linux";
+
       hostModules = {
-        display.gdm    = ./modules/display/gdm.nix;
-        desktop.common = ./modules/desktop/common.nix;
-        desktop.cosmic = ./modules/desktop/cosmic.nix;
+        # Display managers
+        display.gdm          = ./modules/display/gdm.nix;
+        display.cosmicGreeter= ./modules/display/cosmic-greeter.nix;
+
+        # Desktop environments
+        desktop.default      = ./modules/default.nix;  
+        desktop.cosmic       = ./modules/desktop/cosmic.nix;
+        desktop.gnome        = ./modules/desktop/gnome.nix;    
+        desktop.kde          = ./modules/desktop/kde.nix;     
+        desktop.hyprland     = ./modules/desktop/hyprland.nix;
+        desktop.niri         = ./modules/desktop/niri.nix;
+
+        # Host-specific modules
+        host.laptop          = ./modules/host/laptop.nix;
+        host.gaming          = ./modules/host/gaming.nix;
+
+        # Snapper module
+        snapper              = ./modules/snapper.nix;
       };
     in {
       nixosConfigurations = {
@@ -34,55 +50,37 @@
 
         Ryzen5900x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/Ryzen5900x.nix ];
         };
 
         Ryzen5800x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/Ryzen5800x.nix ];
         };
 
         ASUSTuff-A15 = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/ASUSTuff-A15.nix ];
         };
 
         Alex-Desktop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/Alex-Desktop.nix ];
         };
 
         Alex-Laptop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/Alex-Laptop.nix ];
         };
 
         generic = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = {
-            inherit self hyprland niri home-manager;
-            modules = hostModules;
-          };
+          specialArgs = { inherit self hyprland niri home-manager; modules = hostModules; };
           modules = [ ./hosts/generic.nix ];
         };
       };
