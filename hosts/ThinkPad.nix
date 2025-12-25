f2eb35056e57d62640c19c9bf5d7d7972afb349d
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 17:19:11 -0500

    Refactor ThinkPad configuration by simplifying imports
    Removed hardware configuration check and unused variables.

--- hosts/ThinkPad.nix
+++ hosts/ThinkPad.nix
@@ -1,29 +1,22 @@
 { config, lib, pkgs, modules, ... }:
 
-let
-  hwConfigPath = ./hardware-configuration.nix;
-  hwConfigExists = builtins.pathExists hwConfigPath;
-in
 {
   imports = [
-    (if hwConfigExists then import hwConfigPath else {})
-
-    # Correct paths inside modules
     modules.hardware-configuration.thinkpad
     modules.desktop.default
     modules.display.gdm
     modules.desktop.cosmic
   ];
 
+  networking.hostName = "ThinkPad";
+
   users.users.linuxury = {
     isNormalUser = true;
     description = "Main user";
     extraGroups = [ "wheel" "networkmanager" "docker" ];
-    password = "changeme";  # can hash later
     shell = pkgs.fish;
   };
 
-  networking.hostName = "ThinkPad";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   time.timeZone = "America/New_York";
   system.stateVersion = "25.11";
