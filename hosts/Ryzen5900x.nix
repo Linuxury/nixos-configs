b3b30e2dc3b5343210ccaa5da81c8f5ee01b6367
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 18:55:21 -0500

    Update Ryzen5900x.nix

--- hosts/Ryzen5900x.nix
+++ hosts/Ryzen5900x.nix
@@ -1,25 +1,51 @@
-{ ... }:
+{ config, lib, pkgs, modules, ... }:
 
 {
+  #########################
+  # Hardware configuration
+  #########################
   imports = [
-    ../modules/default.nix
-    ../modules/cosmic.nix
-    ../modules/gaming.nix    # ← Gaming enabled here
-    ../modules/snapper.nix   # Optional: enable on desktop too
+    modules.hardware-configuration.ryzen5900x
   ];
 
+  #########################
+  # Desktop environments
+  #########################
+  imports = imports ++ [
+    modules.desktop.default
+    modules.desktop.cosmic
+  ];
+
+  #########################
+  # Display manager
+  #########################
+  imports = imports ++ [
+    modules.display.gdm
+  ];
+
+  #########################
+  # Host-specific modules
+  #########################
+  imports = imports ++ [
+    modules.host.gaming
+  ];
+
+  #########################
+  # Users
+  #########################
+  imports = imports ++ [
+    modules.users.linuxury
+  ];
+
+  #########################
+  # Networking
+  #########################
   networking.hostName = "Ryzen5900x";
 
-  # Your user on this machine
-  users.users.linuxury = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "gamemode" ];
-    initialPassword = "changeme123";
-  };
-
-  security.sudo.wheelNeedsPassword = false;
-
-  # Hardware-specific (RX 7900 XTX + Ryzen 9 5900X)
-  boot.kernelParams = [ "amd_iommu=on" "iommu=pt" ];
-  hardware.cpu.amd.updateMicrocode = true;
+  #########################
+  # Time & Locale
+  #########################
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  time.timeZone = "America/New_York";
+  system.stateVersion = "25.11";
 }
