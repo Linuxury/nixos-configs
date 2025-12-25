132ffc78fa69cb8666b805733b57665e4f0ef819
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 17:31:50 -0500

    Update gdm.nix

--- modules/display/gdm.nix
+++ modules/display/gdm.nix
@@ -1,8 +1,15 @@
-{ lib, config, pkgs, ... }:
+{ lib, ... }:
 
 {
+  services.greetd.enable = lib.mkForce false;
+
+  services.xserver.enable = true;
+
   services.displayManager.gdm = {
     enable = true;
     wayland = true;
   };
+
+  # Safety: ensure no cosmic greeter leaks in
+  services.displayManager.cosmic-greeter.enable = lib.mkForce false;
 }
