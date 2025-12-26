6b0f71f3e7c1e47ec902b407373a96c5c18e4a1d
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 19:21:42 -0500

    Update Ryzen5800x.nix

--- hosts/Ryzen5800x.nix
+++ hosts/Ryzen5800x.nix
@@ -1,21 +1,54 @@
-{ ... }:
+{ config, lib, pkgs, modules, ... }:
 
 {
+  #########################
+  # Hardware configuration
+  #########################
   imports = [
-    ../modules/default.nix
-    ../modules/cosmic.nix
-    ../modules/snapper.nix   # Optional
-    ../modules/gaming.nix    # Optional if she wants gaming
+    modules.hardware-configuration.ryzen5800x
   ];
 
+  #########################
+  # Desktop environments
+  #########################
+  imports = imports ++ [
+    modules.desktop.default
+    modules.desktop.cosmic
+  ];
+
+  #########################
+  # Display manager
+  #########################
+  imports = imports ++ [
+    modules.display.gdm
+  ];
+
+  #########################
+  # Host-specific modules
+  #########################
+  imports = imports ++ [
+    modules.host.gaming
+    modules.host.performance-cpu
+    modules.host.performance-amd    # AMD GPU
+    # modules.host.performance-nvidia   # only uncomment if NVIDIA exists
+  ];
+
+  #########################
+  # Users
+  #########################
+  imports = imports ++ [
+    modules.users.babylinux
+  ];
+
+  #########################
+  # Networking
+  #########################
   networking.hostName = "Ryzen5800x";
 
-  # Wife's user
-  users.users.babylinux = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    initialPassword = "changeme123";
-  };
-
-  security.sudo.wheelNeedsPassword = false;
+  #########################
+  # Time & Locale
+  #########################
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  time.timeZone = "America/New_York";
+  system.stateVersion = "25.11";
 }
