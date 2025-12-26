7a805df5df361a1840362c6932824bce8690713f
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 17:29:09 -0500

    Update Ryzen5900x.nix

--- hosts/Ryzen5900x.nix
+++ hosts/Ryzen5900x.nix
@@ -10,7 +10,7 @@
     hostModules.desktop.default
     hostModules.desktop.cosmic
 
-    hostModules.display.cosmic-greeter
+    hostModules.display.cosmicGreeter
 
     hostModules.host.performance-cpu
     hostModules.host.performance-amd
