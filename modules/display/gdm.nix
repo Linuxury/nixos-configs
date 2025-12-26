a94d093ae76fa72e71e89082888f51288336a0e8
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 18:29:38 -0500

    Update gdm.nix

--- modules/display/gdm.nix
+++ modules/display/gdm.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 
 {
   #########################
   # GDM Display Manager
   #########################
+  environment.systemPackages = with pkgs; [ gdm ];
+
+  # Enable X server for graphical sessions
+  services.xserver.enable = true;
+
+  # Enable GDM
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 }
