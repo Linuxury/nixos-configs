68eebf89188b8433b0e49ec5b663e39fe620511b
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 15:35:19 -0500

    Update cosmic-greeter.nix

--- modules/display/cosmic-greeter.nix
+++ modules/display/cosmic-greeter.nix
@@ -1,32 +1,8 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   #########################
-  # Cosmic Greeter (greetd)
+  # Cosmic greeter (example)
   #########################
-  services.displayManager.cosmic-greeter.enable = true;
-
-  # Required for Wayland/desktop
-  services.xserver.enable = true;
-
-  #########################
-  # Sound & multimedia
-  #########################
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  #########################
-  # Fix greeter startup reliability
-  #########################
-  systemd.services.cosmic-greeter = {
-    wantedBy = lib.mkForce [ "graphical.target" ];
-    restartIfChanged = true;
-    serviceConfig.Restart = "always";
-    serviceConfig.RestartSec = 3;
-  };
+  services.xserver.displayManager.cosmic.enable = true;
 }
