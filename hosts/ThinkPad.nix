96467073404dacebfe3b4ccf71ca976aee02329b
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 15:06:31 -0500

    Update ThinkPad.nix

--- hosts/ThinkPad.nix
+++ hosts/ThinkPad.nix
@@ -1,21 +1,19 @@
-{ config, lib, pkgs, jovianModule ? null, ... }:
+{ config, lib, pkgs, hostModules, jovianModule ? null, ... }:
 
-let
-  optionalJovian = lib.optional (jovianModule != null) [ jovianModule.nixosModules.default ];
-in {
+{
   #########################
   # Hardware configuration
   #########################
   imports = [
-    ./modules/hardware-configuration/thinkpad.nix
-    ./modules/desktop/default.nix
-    ./modules/desktop/cosmic.nix
-    ./modules/display/gdm.nix
-    ./modules/host/laptop.nix
-    ./modules/host/gaming.nix
-    ./modules/host/coding.nix
-    ./modules/users/linuxury.nix
-  ] ++ optionalJovian;
+    hostModules.hardware-configuration.thinkpad
+    hostModules.desktop.default
+    hostModules.desktop.cosmic
+    hostModules.display.gdm
+    hostModules.host.laptop
+    hostModules.host.gaming
+    hostModules.host.coding
+    hostModules.users.linuxury
+  ] ++ lib.optional (jovianModule != null) [ jovianModule.default ];
 
   #########################
   # Networking
