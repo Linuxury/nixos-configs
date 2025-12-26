c23eea1b2ce7d2f343b6c1e0e3b047252a4aef5c
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 20:01:41 -0500

    Update hyprland.nix

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,20 +1,36 @@
-{ inputs, pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  # Hyprland Wayland compositor
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
-  };
+  #########################
+  # Hyprland Desktop Environment
+  #########################
+  services.xserver.enable = true;
 
-  # Required for Wayland apps
-  security.polkit.enable = true;
+  services.desktopManager.hyprland.enable = true;
 
+  #########################
+  # Hyprland Extras
+  #########################
   environment.systemPackages = with pkgs; [
+    hyprland
     waybar
-    rofi-wayland
-    dunst
-    # Add your Hyprland config later in Home Manager
+    mako
+    grim
+    slurp
+    wl-clipboard
+    swaylock
+    swayidle
+    foot        # Terminal
+    wayfire-git # Optional: Wayland compositor tools
   ];
+
+  #########################
+  # Wayland support
+  #########################
+  services.xserver.desktopManager.hyprland.wayland = true;
+
+  #########################
+  # Optional: user scripts or configs
+  #########################
+  # You can add per-user config files or global configs here if needed
 }
