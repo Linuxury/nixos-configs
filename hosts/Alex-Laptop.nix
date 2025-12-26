a50ace425679a211891faf5d71f4d8c8e102092c
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-25 19:27:03 -0500

    Update Alex-Laptop.nix

---
+++ hosts/Alex-Laptop.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, modules, ... }:
+
+{
+  #########################
+  # Hardware configuration
+  #########################
+  imports = [
+    modules.hardware-configuration.alex-laptop
+  ];
+
+  #########################
+  # Desktop environments
+  #########################
+  imports = imports ++ [
+    modules.desktop.default
+    modules.desktop.cosmic
+  ];
+
+  #########################
+  # Display manager
+  #########################
+  imports = imports ++ [
+    modules.display.gdm
+  ];
+
+  #########################
+  # Host-specific modules
+  #########################
+  imports = imports ++ [
+    modules.host.laptop
+    modules.host.gaming
+    # modules.host.performance-amd    # AMD GPU
+    # modules.host.performance-nvidia   # only if NVIDIA exists
+  ];
+
+  #########################
+  # Users
+  #########################
+  imports = imports ++ [
+    modules.users.alex
+  ];
+
+  #########################
+  # Networking
+  #########################
+  networking.hostName = "Alex-Laptop";
+
+  #########################
+  # Time & Locale
+  #########################
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  time.timeZone = "America/New_York";
+  system.stateVersion = "25.11";
+}
