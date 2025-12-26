b5774cd77489d1dd96edb4c53a1a66e7ed95d379
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 17:29:40 -0500

    Update Alex-Desktop.nix

--- hosts/Alex-Desktop.nix
+++ hosts/Alex-Desktop.nix
@@ -10,7 +10,7 @@
     hostModules.desktop.default
     hostModules.desktop.cosmic
 
-    hostModules.display.cosmic-greeter
+    hostModules.display.cosmicGreeter
 
     hostModules.host.laptop
     hostModules.host.gaming
