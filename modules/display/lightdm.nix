6c8f83ecad156ee3c0f64ea8c85cbfa1968da7b8
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 18:43:31 -0500

    Update lightdm.nix

--- modules/display/lightdm.nix
+++ modules/display/lightdm.nix
@@ -4,11 +4,11 @@
   #########################
   # LightDM Display Manager
   #########################
-  environment.systemPackages = with pkgs; [ lightdm ];
-
-  # Enable X server for graphical sessions
   services.xserver.enable = true;
-
-  # Enable LightDM
   services.xserver.displayManager.lightdm.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    lightdm
+    lightdm-gtk-greeter
+  ];
 }
