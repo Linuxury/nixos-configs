fe36bb39cb304c9a2e30db538942adc21c72eb51
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 16:12:17 -0500

    Update babylinux.nix

--- modules/users/babylinux.nix
+++ modules/users/babylinux.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     # Optional description; keep private or local-only
     description = "Milagros Monserrate"; 
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = [ "wheel" "networkmanager" "docker" "video" "audio" ];
     password = "changeme"; # optionally hashed with mkpasswd
     shell = pkgs.fish;
   };
