ea5de9efb8bc6e5910b096f9ce0de858d1c10a72
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-12-26 19:15:53 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -20,103 +20,145 @@
         # Hardware configuration
         #########################
         hardware-configuration.default = ./modules/hardware-configuration/default.nix;
-
         #########################
         # Display managers
         #########################
-        display.gdm           = ./modules/display/gdm.nix;
+        display.gdm = ./modules/display/gdm.nix;
         display.cosmicGreeter = ./modules/display/cosmic-greeter.nix;
-        display.lightdm       = ./modules/display/lightdm.nix;
-        display.sddm          = ./modules/display/sddm.nix;
-
+        display.lightdm = ./modules/display/lightdm.nix;
+        display.sddm = ./modules/display/sddm.nix;
         #########################
         # Desktop environments
         #########################
-        desktop.default      = ./modules/desktop/default.nix;
-        desktop.cosmic       = ./modules/desktop/cosmic.nix;
-        desktop.gnome        = ./modules/desktop/gnome.nix;
-        desktop.kde          = ./modules/desktop/kde.nix;
-        desktop.hyprland     = ./modules/desktop/hyprland.nix;
-        desktop.niri         = ./modules/desktop/niri.nix;
-
+        desktop.default = ./modules/desktop/default.nix;
+        desktop.cosmic = ./modules/desktop/cosmic.nix;
+        desktop.gnome = ./modules/desktop/gnome.nix;
+        desktop.kde = ./modules/desktop/kde.nix;
+        desktop.hyprland = ./modules/desktop/hyprland.nix;
+        desktop.niri = ./modules/desktop/niri.nix;
         #########################
         # Host-specific modules
         #########################
-        host.laptop          = ./modules/host/laptop.nix;
-        host.gaming          = ./modules/host/gaming.nix;
-        host.coding          = ./modules/host/coding.nix;
+        host.laptop = ./modules/host/laptop.nix;
+        host.gaming = ./modules/host/gaming.nix;
+        host.coding = ./modules/host/coding.nix;
         host.performance-cpu = ./modules/host/performance-cpu.nix;
         host.performance-amd = ./modules/host/performance-amd.nix;
         host.performance-nvidia = ./modules/host/performance-nvidia.nix;
-
         #########################
         # Users
         #########################
-        users.linuxury       = ./modules/users/linuxury.nix;
-        users.babylinux      = ./modules/users/babylinux.nix;
-        users.alex           = ./modules/users/alex.nix;
-
+        users.linuxury = ./modules/users/linuxury.nix;
+        users.babylinux = ./modules/users/babylinux.nix;
+        users.alex = ./modules/users/alex.nix;
         #########################
         # Snapper
         #########################
-        snapper              = ./modules/snapper.nix;
+        snapper = ./modules/snapper.nix;
       };
+
+      # Shared modules applied to EVERY host
+      commonModules = [
+        # Home Manager integration
+        home-manager.nixosModules.home-manager
+
+        # Clean, quiet boot + watchdog fixes
+        {
+          boot = {
+            plymouth = {
+              enable = true;
+              theme = "breeze";
+              themePackages = with nixpkgs.legacyPackages.${system}; [
+                breeze-plymouth
+              ];
+            };
+
+            consoleLogLevel = 0;
+            initrd.verbose = false;
+
+            kernelParams = [
+              "quiet"
+              "splash"
+              "loglevel=3"
+              "rd.systemd.show_status=auto"
+              "rd.udev.log_priority=3"
+              "udev.log_priority=3"
+              "vt.global_cursor_default=0"
+              "nowatchdog"
+            ];
+          };
+
+          systemd = {
+            extraConfig = ''
+              DefaultTimeoutStopSec=10s
+            '';
+
+            watchdogs = {
+              runtimeWatchdogSec = 0;
+              rebootWatchdogSec = 0;
+            };
+
+            services.NetworkManager-wait-online.enable = false;
+          };
+        }
+      ];
+
     in {
       nixosConfigurations = {
         ThinkPad = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager hostModules; 
-            };
-          modules = [ ./hosts/ThinkPad.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/ThinkPad.nix ];
         };
 
         Ryzen5900x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/Ryzen5900x.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/Ryzen5900x.nix ];
         };
 
         Ryzen5800x = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/Ryzen5800x.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/Ryzen5800x.nix ];
         };
 
         ASUSTuff-A15 = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/ASUSTuff-A15.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/ASUSTuff-A15.nix ];
         };
 
         Alex-Desktop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/Alex-Desktop.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/Alex-Desktop.nix ];
         };
 
         Alex-Laptop = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/Alex-Laptop.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/Alex-Laptop.nix ];
         };
 
         generic = nixpkgs.lib.nixosSystem {
           inherit system;
-          specialArgs = { 
-            inherit self hyprland niri home-manager; 
-            modules = hostModules; };
-          modules = [ ./hosts/generic.nix ];
+          specialArgs = {
+            inherit self hyprland niri home-manager hostModules;
+          };
+          modules = commonModules ++ [ ./hosts/generic.nix ];
         };
       };
     };
